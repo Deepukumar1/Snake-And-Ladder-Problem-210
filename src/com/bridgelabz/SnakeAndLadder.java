030289f275de39b10ab4c1dfd04a3f069fc8d918
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-10-13 22:46:54 +0530

    check for option added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,9 +4,31 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Snake and Ladder Program");
-		 int startPosition = 0;
-	        int diceNumber = (int) Math.floor(Math.random() * 10) % 6+1;
-	        System.out.println("Dice Roll is:"+diceNumber);
-	        System.out.println(" Start position is: "+startPosition);
+		System.out.println("Player Check Option: ");
+        int position = 0;
+
+        int diceNumber = (int) Math.floor(Math.random() * 10) % 6+1;
+
+        System.out.println("Dice Roll is:"+diceNumber);
+
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+        System.out.println("Player option is : " + option);
+
+        switch (option) {
+            case 1:
+                position = diceNumber + position;
+                System.out.println("Ladder");
+                System.out.println("Position for the player after the ladder is : " + position);
+                break;
+
+            case 2:
+                position = position - diceNumber;
+                System.out.println("Snack");
+                System.out.println("Position for the player after the Snake is : " + position);
+                break;
+
+            default:
+                System.out.println("No play");
+        }
 	}
 }
